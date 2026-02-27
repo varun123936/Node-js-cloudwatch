86e9acb475b8a8b67ebc89c4f2f31e8f2712807d
Rameshwaram Varun Teja <tejavarun689@gmail.com>
2026-02-27 18:11:07 +0530

    commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+// log file path
+const logFile = path.join(__dirname, 'app.log');
+
+function writeLog(message) {
+  const log = `${new Date().toISOString()} - ${message}\n`;
+  fs.appendFileSync(logFile, log);
+}
+
+app.get('/', (req, res) => {
+  writeLog('Home API hit');
+  res.send('Node App Running 🚀');
+});
+
+app.get('/error', (req, res) => {
+  writeLog('Error endpoint triggered');
+  res.status(500).send('Error triggered');
+});
+
+app.listen(PORT, () => {
+  writeLog(`Server started on port ${PORT}`);
+  console.log(`Server running on ${PORT}`);
+});
