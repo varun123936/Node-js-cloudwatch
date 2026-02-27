692495a1b879524433a130c95470fcb7fcb6ae89
Rameshwaram Varun Teja <tejavarun689@gmail.com>
2026-02-27 18:14:17 +0530

    commit 2

--- app.js
+++ app.js
@@ -8,22 +8,19 @@ const PORT = 3000;
 // log file path
 const logFile = path.join(__dirname, 'app.log');
 
-function writeLog(message) {
-  const log = `${new Date().toISOString()} - ${message}\n`;
-  fs.appendFileSync(logFile, log);
-}
+function writeLog(message) { const timestamp = new Date().toLocaleString('en-IN', { timeZone: 'Asia/Kolkata' }); const log = `${timestamp} - ${message}\n`; fs.appendFileSync(logFile, log); }
 
 app.get('/', (req, res) => {
-  writeLog('Home API hit');
-  res.send('Node App Running 🚀');
+    writeLog('Home API hit');
+    res.send('Node App Running 🚀');
 });
 
 app.get('/error', (req, res) => {
-  writeLog('Error endpoint triggered');
-  res.status(500).send('Error triggered');
+    writeLog('Error endpoint triggered');
+    res.status(500).send('Error triggered');
 });
 
 app.listen(PORT, () => {
-  writeLog(`Server started on port ${PORT}`);
-  console.log(`Server running on ${PORT}`);
+    writeLog(`Server started on port ${PORT}`);
+    console.log(`Server running on ${PORT}`);
 });
